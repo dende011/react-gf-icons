d4cacbde9de6abb304c396d288cd01cd7b3d9de6
roddytc <ricar.tig@gmail.com>
2023-06-16 17:16:28 -0500

    feature:cambiando nombre de iconos

---
+++ src/IconContratista.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+
+function SvgIconContratista(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      id="Layer_1"
+      x={0}
+      y={0}
+      width="1em"
+      height="1em"
+      viewBox="0 0 40 40"
+      xmlSpace="preserve"
+      {...props}
+    >
+      <image
+        id="image0"
+        width={40}
+        height={40}
+        x={0}
+        y={0}
+        xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAoCAMAAAC7IEhfAAAAIGNIUk0AAHomAACAhAAA+gAAAIDo AAB1MAAA6mAAADqYAAAXcJy6UTwAAAHIUExURQAAAEeJxEeIyEiGxUuMyU2Oymij3Wej20uMyUyL yUSJyTOZzEuMyU2Myniw53ew50uNykuKyUaNxlya1V2a1UuHw2mj3Wii3UuKyV2a1V2b1kyKykaH yEuKyXix53ev50yMykaFxUuMykmGyUeJyGik3EiJx0yMy02MykyMykuLyUmKyEiJyFV/1EyLyUqM yUyMyVub1UqKyEqKyUWJxmmh12Kc1EaIx06LyVCNyUmJyE2LyU2NyFKQylGOykmIx3Sn13Gl1kmJ xkx/zE2NyU6NyUSJxEqLyEqKx0eEzE+NylCNyVSRy1SQykiJx3+s2H2r10mJxkh/yE2LyE6JxE6N ynup13qp1kyMyEiRtk+OyqvJ5anH5EaDyk6MyanI5ajH5EiIyEuExnmo1nen1VCNyU2NyU2MyICt 2E6NyE2NyViTzEaGxlOQykuLyEeIx5jM/ZfM/JfL/JTJ+pjL/ZTI+lOSz4C57oC47ozC9YzC9EqL yUuLyY/F94/E9////02MykyMyWyi1Z7A4k+NyZ/B4nSo2K7O7FCOyq/P7bLR7aDB4rHR7fn7/ff6 /efw+Obv97/W7L3V6+nx+Ojw+PH2+8LY7fX4/GH7PFgAAABvdFJOUwAneTXK9Pf39ck0BZ71/v71 nBL19RH5+Z709Zwzyv7+yCj1Jnr3d8vJ5+b7/AaQj4/18O9x/Ptl0Mz78fDNzGX09GQK4uAa/f0Z 7Oq8uGr082gO9Q2C9PR/B9n7+x3q+/scG/T02ID09H6Q9EjJ96ECMjIAAAABYktHRH9Iv3HlAAAA CXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH5wYQFDsC0V+mHQAAAZdJREFUOMud0/VPAzEYgOEC gzEOH+7uMNx9uMuw4T60hQnuDIY7+3fhdhu5td9tCe8PTZN70rRND6F/h225ZR6emPyGPT1cMi+Z t48A5b4KP0nG+QcE7hABkt2g4BAOdqEypd5m7IM+TBEOrieLMBAxJIZIBbRmVLTNiSAxxMQC5wjQ ExoSY3AcA+MTCAtJYhLtkr13ILibkkrBtHT7J2L6G/jkGRTM3CNg+1kUzD6A4WEOBXONMDTmObH8 giOMTSwzYXysyhfBwqITItGJUiWCxZKOkNMSEcTEReJHjF3mdJzSMnwGhMsrqOupPIfhRRUFqy95 eHVtdhBhim+qKVhzy0MLtjigMMV3tRSsu+eh2fLggMIUP9ZTsKER3mNTMwXVLTBsVdMvt639iXXP HZ3sT9P1wsLXbtahnjcWvvcCsK+fhQODABwa/qDdy7AGgGhklIZj45BDE9pJZzelnQAhmp6ZFbu5 mXkk0YJ28fNvf0vLbUgybmVV98WzL93qGodcpVnf+LZavzfWNchdm1vb21ubbplkP60eYckGyz5y AAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDIzLTA2LTE2VDIwOjU5OjAyKzAwOjAwh7wwCgAAACV0RVh0 ZGF0ZTptb2RpZnkAMjAyMy0wNi0xNlQyMDo1OTowMiswMDowMPbhiLYAAAAodEVYdGRhdGU6dGlt ZXN0YW1wADIwMjMtMDYtMTZUMjA6NTk6MDIrMDA6MDCh9KlpAAAAAElFTkSuQmCC"
+      />
+    </svg>
+  );
+}
+
+export default SvgIconContratista;
